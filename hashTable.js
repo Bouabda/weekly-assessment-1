74b67119aeab71d7d2eeba5062e1a09e56faecac
bouabda <abdaoui.wala@gmail.com>
2020-03-16 10:50:57 +0100

    assessment I

--- hashTable.js
+++ hashTable.js
@@ -6,18 +6,55 @@ var HashTable = function() {
   this._limit = 8;
 }
 
-
+//  insertion
 HashTable.prototype.insert = function(key, value) {
+  
+  let index = this.hashFunc(key, this._limit) 
+  let bucket = this._storage[index];
+  
+  for (let i = 0; i < bucket.length; i++) {
+      let tuple = bucket[i];
+      if (tuple[0] === key) {
+      let oldValue = tuple[1];
+      tuple[1] = value;
+      return oldValue;
+    }
+  }
+  bucket.push([key, value]);
+    this._storage.push([index, bucket]);
+    this._count++;
 };
 
 
 HashTable.prototype.remove = function(key) {
+  let index = this.hashFunc(key, this._limit) 
+  let bucket = this._storage[index];
+  
+    for (let i = 0; i < bucket.length; i++) {
+      let tuple = bucket[i];
+      if (tuple[0] === key) {
+        bucket.splice(i, 1);
+        this._count--;
+        return tuple[1];
+    }
+    return undefined ;
+  }
+
 };
-
-
 
 HashTable.prototype.retrieve = function(key) {
-};
+  let index = this.hashFunc(key, this._limit)
+  let bucket = this._storage[index];
+   
+  for (let i = 0; i < bucket.length; i++) {
+      let tuple = bucket[i];
+      if (tuple[0] === key) {
+        return tuple[1];
+      }
+      return undefined;
+    }
+  };
+
 
 
 HashTable.prototype.hashFunc = function(str, max) {
--- queueFromStacks.js
+++ queueFromStacks.js
@@ -23,6 +23,30 @@ class Stack {
 // Implement a Queue using Stacks using EcmaScript 6 Syntax
 ////////////////////////////////////////////////////////////
 
+// takes an array of data : FIFO 
 class Queue {
-
-}
+  constructor() {
+      this.rear = 0 ;
+      this.front = 0 ;
+      this.storage = [];
+  }
+  enqueue(value) {
+  this.storage[this.rear] = value;
+  this.rear ++
+  };
+  
+  dequeue() {
+    var deletedValue = this.storage[this.front] ;
+      if (this.rear > 0) {
+       delete this.storage[this.front] ;
+       this.storage[this.front] = this.storage[this.rear-1] ;
+       delete this.storage[this.rear-1] ;
+       this.rear -- ;
+      } return deletedValue ;
+  
+  }
+  
+  size () {
+    return this.rear - this.front ;
+    }
+  }
